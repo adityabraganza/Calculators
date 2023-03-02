259446943ddcc5d9c78782d3e377ddbddc2f7c9c
Aditya Braganza <aditya.braganza@gmail.com>
2023-03-02 21:09:06 +0530

    Creating main.py
    Beta 0.1

---
+++ main.py
@@ -0,0 +1,14 @@
+from Calculators.stdDev import run_stdDEV
+from Calculators.quadEq import run_quadEq
+import tkinter as tk
+
+GUI_Main = tk.Tk()
+GUI_Main.title("Home")
+
+but_StdDEV = tk.Button(GUI_Main, text="Standard Deviation", width=20, command=run_stdDEV)
+but_StdDEV.grid(row=0, column=0)
+
+but_quadEq = tk.Button(GUI_Main, text="Quadratic Equation", width=20, command=run_quadEq)
+but_quadEq.grid(row=0, column=1)
+
+GUI_Main.mainloop()
